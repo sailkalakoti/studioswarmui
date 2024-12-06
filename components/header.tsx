a65eff2c17d82525c01ffaa3d4c3264aa00d1cfe
datamodelsviz <datamodelsviz@gmail.com>
2024-12-06 12:55:44 +0530

    Updated header color

--- components/header.tsx
+++ components/header.tsx
@@ -5,11 +5,11 @@ import { SearchModal } from "./search-modal";
 
 const Header = () => {
   return (
-    <header className="p-2 bg-gray-100 border-b border-gray-200 fixed top-0 w-full z-10">
+    <header className="p-2 fixed top-0 w-full z-10 bg-gradient-to-r from-[#002856] to-[#1a4c8b] shadow-md">
       <div className="flex items-center justify-between mx-auto">
         <Link href="/dashboard" className="h-5 flex items-center gap-1.5">
-          <Logo color="brand" />
-          <span className="font-semibold text-[18px] text-[#002856]">StudioSwarm</span>
+          <Logo color="white" />
+          <span className="font-semibold text-[18px] text-white">StudioSwarm</span>
         </Link>
         <div className="flex-1 flex justify-center">
           <SearchModal />
